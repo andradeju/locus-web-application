9b394d75c47f7e90d1286c62712c477f2a766bce
andradeju <julliana.andrade08@gmail.com>
2026-04-01 23:30:26 -0300

    feat: initial project setup

---
+++ src/main/java/com/locus/webApplication/LocusWebApplication.java
@@ -0,0 +1,13 @@
+package com.locus.webApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocusWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocusWebApplication.class, args);
+	}
+
+}
