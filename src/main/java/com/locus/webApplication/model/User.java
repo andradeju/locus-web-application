1ca9cbb6e9602ba203fc2eb3a187a9832c4474ce
andradeju <julliana.andrade08@gmail.com>
2026-04-02 21:12:20 -0300

    feat(model): create User entity with Role enum

--- src/main/java/com/locus/webApplication/model/User.java
+++ src/main/java/com/locus/webApplication/model/User.java
@@ -1,4 +1,43 @@
 package com.locus.webApplication.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Address> addresses;
 }
