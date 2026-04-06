380d36d83f99c8958277867451f45df6517a7203
andradeju <julliana.andrade08@gmail.com>
2026-04-06 19:43:24 -0300

    chore: remove data.sql seed file

--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users (id, name, cpf, birth_date, password_hash, role)
-VALUES (
-           gen_random_uuid(),
-           'Admin',
-           '98765432100',
-           '1990-01-01',
-           '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy',
-           'ADMIN'
-       ) ON CONFLICT DO NOTHING;
