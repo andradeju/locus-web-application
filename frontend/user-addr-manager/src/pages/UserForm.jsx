bb4d12f931a2d969bec0c6f17650eafc211d8529
andradeju <julliana.andrade08@gmail.com>
2026-05-06 14:25:12 -0300

    refactor: extract CPF validation

--- frontend/user-addr-manager/src/pages/UserForm.jsx
+++ frontend/user-addr-manager/src/pages/UserForm.jsx
@@ -1,32 +1,7 @@
 import { useState } from 'react';
 import { createUser } from '../services/userService';
 import FormField from '../components/FormField';
-
-function formatCpf(value) {
-  const digits = value.replace(/\D/g, '').slice(0, 11);
-  return digits
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d)/, '$1.$2')
-    .replace(/(\d{3})(\d{1,2})$/, '$1-$2');
-}
-
-function validateCpf(cpf) {
-  const digits = cpf.replace(/\D/g, '');
-  if (digits.length !== 11) return false;
-  if (/^(\d)\1{10}$/.test(digits)) return false;
-
-  let sum = 0;
-  for (let i = 0; i < 9; i++) sum += parseInt(digits[i]) * (10 - i);
-  let remainder = (sum * 10) % 11;
-  if (remainder === 10 || remainder === 11) remainder = 0;
-  if (remainder !== parseInt(digits[9])) return false;
-
-  sum = 0;
-  for (let i = 0; i < 10; i++) sum += parseInt(digits[i]) * (11 - i);
-  remainder = (sum * 10) % 11;
-  if (remainder === 10 || remainder === 11) remainder = 0;
-  return remainder === parseInt(digits[10]);
-}
+import  { formatCpf, validateCpf } from "../utils/validators.js";
 
 const initialForm = { name: '', cpf: '', birthDate: '', password: '' };
 
